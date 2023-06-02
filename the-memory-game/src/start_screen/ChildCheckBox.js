b5b69fd2dee4caf83d54d18c9e0b01a5727e822e
AakashAxe <aakashv1942@gmail.com>
2023-06-02 16:03:48 +1000

    Checkbox render issue, commiting for new branch

--- the-memory-game/src/start_screen/ChildCheckBox.js
+++ the-memory-game/src/start_screen/ChildCheckBox.js
@@ -1,10 +1,25 @@
 
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import React from 'react';
+import React, {useState} from 'react';
 
-const ChildCheckBox = ({diffs, setDiff}) => {
+/*
+ISSUE: 
+The 3 Checkboxes dont render whenever i click on one.
+
+when i click an option the rest should be disselected.
+
+*/
+
+const ChildCheckBox = () => {
     
+    const [diffs, setDiff] = useState(
+        [
+           {id: "1", name: "Easy", selected: false} ,
+           {id: "2", name: "Medium", selected: false},
+           {id: "3", name: "Hard", selected: false}  
+        ]
+    )
     console.log(diffs)
     
     const handleChange = (id) => {
@@ -30,10 +45,13 @@ const ChildCheckBox = ({diffs, setDiff}) => {
     return (    
             <div>
             {
+                
                 diffs.map((element, index)=>{
-                    return (  
+                    console.log("PLS RENDER")
+                    return ( 
+
                     <FormControlLabel 
-                    control={element.selected ? <Checkbox defaultChecked /> : <Checkbox />} 
+                    control={<Checkbox value={element.selected}/>} 
                     label={element.name}
                     id={element.id} 
                     key={index}
--- the-memory-game/src/start_screen/StartScreen.js
+++ the-memory-game/src/start_screen/StartScreen.js
@@ -6,20 +6,12 @@ import ChildCheckBox from './ChildCheckBox';
 
 const StartScreen = () => {
 
-    const [levels, setLevels] = useState(
-        [
-           {id: "1", name: "Easy", selected: false} ,
-           {id: "2", name: "Medium", selected: false},
-           {id: "3", name: "Hard", selected: false}  
-        ]
-    )
 
     return (
         
         <div>
-            {console.log(levels)}
             <FormGroup>
-                <ChildCheckBox diffs={levels} setDiff={setLevels}/>
+                <ChildCheckBox/>
             </FormGroup>
             <Link to="/game"><Button variant="contained">Start Game</Button></Link>
         </div>
